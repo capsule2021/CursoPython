659977d3177a0f834aa0c664dd9f717e892b6c53
Jonatas Anunciaçao Dias <capsulebit@gmail.com>
2024-02-22 17:38:57 -0300

    Formatação de strings com o método format

---
+++ aula12.py
@@ -0,0 +1,14 @@
+nome = 'Jonatas Dias'
+altura = 1.75
+peso = 70
+imc = ...
+# imc = peso / altura **2
+ 
+imc = peso / (altura * altura)
+print (nome, 'tem',altura, ' de altura',)
+print ('pesa',peso, 'quilos e seu imc é,')
+print (imc)
+
+#luiz Otavio tem 1.80 de altura 
+#pesa 95 quilos e seu IMC é 
+# 29.320987654320987
