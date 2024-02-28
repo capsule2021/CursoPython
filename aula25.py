7da3d995206e15982b524222bbea945a402a8159
Jonatas Anunciaçao Dias <capsulebit@gmail.com>
2024-02-28 09:52:57 -0300

    Interpolação de string com % em Python

---
+++ aula25.py
@@ -0,0 +1,13 @@
+"""
+Interpolacão básica de strings
+s - string
+e i - int
+f - float
+x e x - Hexadecimal (ABCDEF01233456789)
+
+"""
+
+nome = 'Jonatas'
+preco = 1000.95897643
+variavel = '%s, o preco e R$%.2f' % (nome,preco)
+print(variavel)
