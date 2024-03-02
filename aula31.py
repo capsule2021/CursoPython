61834ebeb1a419b05f98066887d76dcd5f595572
Jonatas Anunciaçao Dias <capsulebit@gmail.com>
2024-03-02 12:24:01 -0300

    A identidade do valor que está na memória

---
+++ aula31.py
@@ -0,0 +1,22 @@
+"""
+Flag (Bnadeira) - Marcar um local
+None = Nao valor
+is e is not = é ou nao é  (tipo,valor,identidade)
+id = Identidade
+"""
+
+v1 = 'a'
+v2 = 'a'
+v3 = 'b'
+print(id(v1))
+print(id(v2))
+print(id(v3))
+#condicao = false
+
+# condicao
+
+# if condicao:
+ #   print('faça algo')
+#else:
+ ##   print('NÃO FAÇA ALGO')
+  
