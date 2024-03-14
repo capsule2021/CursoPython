a600cba4956b3fde749d7f6d52d6d19d57e66ec3
Jonatas Anunciaçao Dias <capsulebit@gmail.com>
2024-03-14 01:09:37 -0300

    Exercício - exiba os índices da lista (aula com solução)

---
+++ aula51.py
@@ -0,0 +1,24 @@
+"""
+Exercício
+Exiba os índices da lista
+0 Maria
+1 Helena
+2 Luiz
+"""
+# lista = ['Maria', 'Helena', 'Luiz']
+# lista.append('João')
+
+
+# indices = range(len(lista))
+
+# for indice in indices:
+#     print(indice, lista[indice], type(lista[indice]))
+
+
+pc = ['Tony', 'Carlos', 'Wilson','Jonatas']    
+pc.append('Icaro')
+
+indices = range(len(pc))
+for indice in indices:
+    print(indice,pc[indice], type(pc[indice]))
+    
